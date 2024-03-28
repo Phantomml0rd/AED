4b2775c4a335f978d6e510dca7757d2606d0b078
Phantomml0rd <153093167+Phantomml0rd@users.noreply.github.com>
2024-03-28 11:52:37 -0300

    Update 01.cpp

--- POO/LISTA_02/01.cpp
+++ POO/LISTA_02/01.cpp
@@ -1,1 +1,6 @@
-
+#QUESTÃO 01
+#a) É um tipo de programação baseado no conceit de objetos que podem armazenar dados em formas de estruturas, também conhecidos como atributos, e códigos, na forma de procedimentos, também conhecidos como métodos.
+#b) Uma classe é uma forma de definir um tipo de dado em uma linguagem orientada a objeto. Ela é formada por dados e comportamentos. Por exemplo vamos criar uma classe chamada empresa, ela será estrutura que irá
+# que irá definir o comportamento do objeto e os atributos, os atributos que empresa terá como CNPJ, endereço, número de funcionários, filiais e etc podem ser públicos, privados ou protegidos.
+#c) Uma instância é um objeto específico criado a partir de uma classe. Quando você cria uma classe em POO, você está apenas definindo um modelo ou um tipo de objeto. Para realmente usar esse modelo e trabalhar 
+# com os dados e comportamentos definidos na classe, é preciso criar instâncias dessa classe.
