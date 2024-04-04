883418c1e5b3da665a27e1a69e7aaa9c3f19ab28
Phantomml0rd <153093167+Phantomml0rd@users.noreply.github.com>
2024-04-04 12:01:56 -0300

    Update lista_3_1.cpp

--- POO/LISTA_03/lista_3_1.cpp
+++ POO/LISTA_03/lista_3_1.cpp
@@ -1,1 +1,58 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
+class Veiculo {
+ public:
+    Veiculo (int peso_, int ano_, string marca_, string cor_){
+      peso=peso_;
+      ano=ano_;
+      marca=marca_;
+      cor=cor_;
+    }  
+void setPeso(int peso_){
+  peso=peso_;
+}
+void setAno(int ano_){
+  ano=ano_;
+}
+void setMarca(string marca_){
+  marca=marca_;
+}
+void setCor(string cor_){
+  cor=cor_;
+}
+int getPeso(){
+  return peso;
+}
+int getAno(){
+  return ano;
+}
+string GetMarca(){
+  return marca;
+}
+string getCor(){
+  return cor;
+}
+private:
+int peso;
+int ano;
+string marca;
+string cor;
+
+};
+  
+int main (){
+  Veiculo veiculo(1500, 1990, "Mitsubishi", "Prata");
+  cout<< "Ano:" << veiculo.getAno() << endl;
+  cout<< "Peso:" << veiculo.getPeso() << endl;
+  cout<< "Marca:"<< veiculo.GetMarca() << endl;
+  cout<< "Cor:" << veiculo.getCor() << endl;
+veiculo.setAno(5);
+  cout<< "Exibindo informações sobre o veiculo após alteações" << veiculo.getAno()<<endl;
+  cout<< "Ano:"<< veiculo.getAno() << endl;
+  
+  
+  return 0;
+  
+}
