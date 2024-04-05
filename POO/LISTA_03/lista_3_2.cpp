4ff152d994023eb3594efee731bce68c652b7197
Phantomml0rd <153093167+Phantomml0rd@users.noreply.github.com>
2024-04-04 23:07:08 -0300

    Update lista_3_2.cpp

--- POO/LISTA_03/lista_3_2.cpp
+++ POO/LISTA_03/lista_3_2.cpp
@@ -1,1 +1,52 @@
+#include <iostream>
+#include <string>
 
+using namespace std;
+
+class Pessoa {
+protected:
+    string nome;
+
+public:
+    Pessoa(string nome_) : nome(nome_) {}
+
+    string getNome() const {
+        return nome;
+    }
+};
+
+class Empregado : public Pessoa {
+protected:
+    float salarioBase;
+
+public:
+    Empregado(string nome_, float salarioBase_) : Pessoa(nome_), salarioBase(salarioBase_) {}
+
+    virtual float calcularSalario() const {
+        return salarioBase;
+    }
+};
+
+class Vendedor : public Empregado {
+private:
+    float valorVendas;
+    float comissao;
+
+public:
+    Vendedor(string nome_, float salarioBase_, float valorVendas_, float comissao_)
+        : Empregado(nome_, salarioBase_), valorVendas( valorVendas_), comissao(comissao_) {}
+
+    virtual float calcularSalario() const override {
+        return salarioBase + (valorVendas * comissao / 100.0);
+    }
+};
+
+int main() {
+    Vendedor vendedor("Pedro", 5035.40, 15.500, 2.500); 
+
+    float salarioVendedor = vendedor.calcularSalario();
+
+    cout << "Nome do Vendedor: " << vendedor.getNome() << endl;
+    cout << "Salario do Vendedor: R$ " << salarioVendedor << endl;
+
+    return 0;
