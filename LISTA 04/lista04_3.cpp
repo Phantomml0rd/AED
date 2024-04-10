e1b75919c56fdaf1501ca6cfaa7c4f8b99c1d57e
Phantomml0rd <153093167+Phantomml0rd@users.noreply.github.com>
2024-04-10 15:34:44 -0300

    Update lista04_3.cpp

--- LISTA 04/lista04_3.cpp
+++ LISTA 04/lista04_3.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int peqseq(int val){
+int vector[10] = {1,2,3,4,5,6,7,8,9,10};
+int indice = 0;
+  
+  while (indice < 10 ){
+    if (vector[indice] == val){
+      return indice;
+    }
+    indice++;
+  }
+  return -1;
+}
+int main(){
+  int posição;
+  cout<< "possuimos um vetor de valor 10 numerado de 1 a 10 digite sua escolha: ";
+  cin>> posição;
+  cout<< "o valor da posição é: " << peqseq(posição);
+    
+
+      return 0;
+  }
 
